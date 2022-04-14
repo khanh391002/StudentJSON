68d9be4b020747500f5d3f226f0eee5f27a84eb8
unknown <khanhnguyen3901@gmail.com>
2022-04-14 22:53:15 +0700

    first commit

---
+++ studentJson/src/main/java/com/example/studentJson/StudentJsonApplication.java
@@ -0,0 +1,64 @@
+package com.example.studentJson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+//@SpringBootApplication
+public class StudentJsonApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(StudentJsonApplication.class, args);
+//	}
+//
+//}
+/**
+ * @author Crunchify.com
+ * How to write JSON object to File in Java?
+ */
+	private static FileWriter file;
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+
+		// JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
+		JSONObject obj = new JSONObject();
+		obj.put("Name", "Khanh");
+		obj.put("Class", "TT32h2");
+
+		JSONArray subject = new JSONArray();
+		subject.add("subject: Python co ban");
+		subject.add("subject: Java co ban");
+		subject.add("subject: Lap trinh .NET");
+		obj.put("subject List", subject);
+		try {
+
+			// Constructs a FileWriter given a file name, using the platform's default charset
+			file = new FileWriter("data.txt");
+			file.write(obj.toJSONString());
+			CrunchifyLog("Ghi thành công vào file");
+			CrunchifyLog("\nĐối tượng JSON: " + obj);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+				file.flush();
+				file.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	static public void CrunchifyLog(String str) {
+		System.out.println("OKE RỒI !");
+	}
+
+}
---
+++ studentJson/src/test/java/com/example/studentJson/StudentJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentJson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
